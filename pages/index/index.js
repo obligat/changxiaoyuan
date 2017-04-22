6254dc53cc06bf498f7ef277d71fdd55638d1026
obligat <duzi1996@qq.com>
2017-04-22 20:56:00 +0800

    add course

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,21 @@
 //index.js
-//获取应用实例
 var app = getApp()
+var sort = require('../../utils/sort')
 Page({
   data: {
     username: wx.getStorageSync('username') || '',
     searchInput: '',
     opacity: 1,
+    courseMessage: wx.getStorageSync('courseMessage') || '',
   },
   handleKeyInput(e) {
     this.setData({
       searchInput: e.detail.value
+    })
+  },
+  handleLogin() {
+    wx.navigateTo({
+      url: '../loginZhiHuiClass/loginZhiHuiClass'
     })
   },
   clearSearchInput() {
@@ -21,6 +27,37 @@ Page({
     console.log('index  onload : ' + this.data.username)
   },
   onShow() {
-    console.log('index onshow: ' + this.data.username)
+    if (!this.data.username && wx.getStorageSync('username')) {
+      this.setData({
+        username: wx.getStorageSync('username')
+      })
+      var that = this
+      wx.request({
+        url: 'https://wwwxinle.cn/wechatapp/schedule.php',
+        data: {
+          session_3rd: wx.getStorageSync('session_3rd')
+        },
+        method: 'POST',
+        header: {
+          "content-type": "application/x-www-form-urlencoded"
+        },
+        success: function (res) {
+          console.log(res)
+          var filterResult = sort.filterByWeekNum(res.data.Obj)
+          var sortResult = sort.sortByJT_NO(filterResult)
+          wx.setStorageSync('courseMessage', sort.formatWeek(sortResult))
+          that.setData({
+            courseMessage: wx.getStorageSync('courseMessage')
+          })
+          console.log('index onload ----- courseMessage')
+          console.log(that.data.courseMessage)
+        }
+      })
+    } else if (this.data.username && !wx.getStorageSync('username')) {
+      this.setData({
+        username: ''
+      })
+      console.log('index onshow: ' + this.data.username)
+    }
   }
 })
