b5009bc4a74bdda023dcc252397a3dc7315f2d95
obligat <duzi1996@qq.com>
2017-04-23 12:37:22 +0800

    borrow page init data

--- pages/borrow/borrow.js
+++ pages/borrow/borrow.js
@@ -1,19 +1,36 @@
 // pages/borrow/borrow.js
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+    username: wx.getStorageSync('libInfo').username || '',
+    borrowInfo: wx.getStorageSync('libInfo').borrowInfo || ''
+  },
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    console.log('borrow onload')
+    console.log(this.data.borrowInfo)
+    // wx.clearStorageSync()
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
+    console.log('borrow onready')
+    console.log(wx.getStorageInfoSync())
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
+    console.log('borrow onshow')
+    if (!this.data.username && wx.getStorageSync('libInfo').username) {
+      this.setData({
+        username: wx.getStorageSync('libInfo').username,
+        borrowInfo: wx.getStorageSync('libInfo').borrowInfo
+      })
+    }
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
+    console.log('borrow onhide')
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
+    console.log('borrow onunload')
   }
 })
