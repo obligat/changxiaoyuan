1f1899dce6ed1b9062a538d27ca95a999491dacb
obligat <duzi1996@qq.com>
2017-04-27 16:48:03 +0800

    UI borrow_bug

--- pages/borrow/borrow.js
+++ pages/borrow/borrow.js
@@ -12,18 +12,33 @@ Page({
     })
   },
   onLoad: function (options) {
-    var borrowInfo = wx.getStorageSync('libInfo').borrowInfo
-    borrowInfo.forEach(function (item) {
-      var days = util.getIntervalDays(item.time)
-      if (days >= 0) {
-        Object.assign(item, { returnInfo: '剩余 ' + days + ' 天' })
-      } else {
-        Object.assign(item, { returnInfo: '超期 ' + (-days) + ' 天' })
-      }
-    })
+    var that = this
+    var username = wx.getStorageSync('libInfo').username
+    var password = wx.getStorageSync('libInfo').password
     this.setData({
-      username: wx.getStorageSync('libInfo').username,
-      borrowInfo: borrowInfo
+      username: username
+    })
+    wx.request({
+      url: 'https://libapi.changxiaoyuan.com/index.php?do=user&username=' + username + '&password=' + password,
+      data: {},
+      method: 'GET',
+      success: function (res) {
+        console.log(res)
+        if (res.data) {
+          var borrowInfo = res.data.book
+          borrowInfo.forEach(function (item) {
+            var days = util.getIntervalDays(item.time)
+            if (days >= 0) {
+              Object.assign(item, { returnInfo: '剩余 ' + days + ' 天' })
+            } else {
+              Object.assign(item, { returnInfo: '超期 ' + (-days) + ' 天' })
+            }
+          })
+          that.setData({
+            borrowInfo: borrowInfo
+          })
+        }
+      }
     })
   },
   handleRenew(e) {
@@ -34,13 +49,10 @@ Page({
     wx.request({
       url: 'https://libapi.changxiaoyuan.com/index.php?do=renew&ctrlid=' + bookid,
       data: {},
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
+      method: 'GET',
       success: function (res) {
-        // success
         var currentBook = borrowInfo.find(function (item) {
           if (item.bookid == bookid) {
-            item.time = '2017/05/24'
             item.returnInfo = '剩余 30 天'
             item.renew = false
             return item
@@ -52,27 +64,7 @@ Page({
         wx.showToast({
           title: '续借成功'
         })
-      },
-      fail: function (res) {
-        // fail
-      },
-      complete: function (res) {
-        // complete
       }
     })
-  },
-  onReady: function () {
-    // 页面渲染完成
-    console.log('borrow onready')
-    console.log(wx.getStorageInfoSync())
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
   }
 })
--- pages/loginLibrary/loginLibrary.js
+++ pages/loginLibrary/loginLibrary.js
@@ -15,8 +15,7 @@ Page({
       wx.request({
         url: 'https://libapi.changxiaoyuan.com/index.php?do=user&username=' + username + '&password=' + password,
         data: {},
-        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-        // header: {}, // 设置请求的 header
+        method: 'GET',
         success: function (res) {
           console.log(res)
           if (res.data.msg) {
@@ -27,8 +26,7 @@ Page({
             try {
               wx.setStorageSync('libInfo', {
                 username: username,
-                password: password,
-                borrowInfo: res.data.book
+                password: password
               })
             } catch (e) {
               console.log(e)
@@ -40,26 +38,10 @@ Page({
             })
             setTimeout(function () {
               wx.redirectTo({
-                url: '../borrow/borrow',
-                success: function (res) {
-                  // success
-                },
-                fail: function (res) {
-                  // fail
-                },
-                complete: function (res) {
-                  // complete
-                }
+                url: '../borrow/borrow'
               })
             }, 1000)
           }
-        },
-        fail: function (res) {
-          // fail
-          console.log('login lib fail.')
-        },
-        complete: function (res) {
-          // complete
         }
       })
     }
@@ -90,22 +72,5 @@ Page({
       password: '',
       errorMsg: ''
     })
-  },
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-    console.log('loginLibrary onUnload:-----------')
-    console.log(wx.getStorageSync('libInfo'))
   }
 })
